777102ced9c78cfeb478dda491eb3a39a1f59713
yhawbee1 <antwibrian82@gmail.com>
2024-09-16 12:00:21 +0000

    Create listing card component

---
+++ src/app/Pages/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ListingCardComponent } from '../../shared/components/listing-card/listing-card.component'; 
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [ListingCardComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
+
 import { CommonModule } from '@angular/common';
 
 @Component({
