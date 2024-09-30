c82c5dc477ed1f888c9123919884177b6368fc3d
yhawbee1 <antwibrian82@gmail.com>
2024-09-30 09:53:45 +0000

    Create carousel component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,20 +1,14 @@
 import { Component } from '@angular/core';
-import { AuthService } from '@auth0/auth0-angular';
-import { CommonModule } from '@angular/common';
-import { DOCUMENT } from '@angular/common';
-import { Inject } from '@angular/core';
-import { OnInit } from '@angular/core';
 import { ListingCardComponent } from '../../shared/components/listing-card/listing-card.component'; 
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, ListingCardComponent],
+  imports: [ListingCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent{
-
-  constructor(public auth: AuthService, @Inject(DOCUMENT) public document: Document) {}
+export class HomeComponent {
 
 }
