43e2d2c9545f375750563e5b2ba4e713bffeac0e
yhawbee1 <antwibrian82@gmail.com>
2024-10-07 09:16:52 +0000

    Create contact us page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './static/pages/home/home.component';
 import { DashbaordComponent } from './features/dashbaord/dashbaord.component';
 import { UsedTeslaForSaleComponent } from './features/listings/pages/used-tesla-for-sale/used-tesla-for-sale.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
   {path:'', redirectTo:'home', pathMatch:'full'},
   {path:'used-tesla-for-sale', component:UsedTeslaForSaleComponent},
+  {path:'contact', component:ContactComponent},
   {path:'dashboard', component:DashbaordComponent},
   {path:'home', component:HomeComponent},
   {path:'**', redirectTo:'home', pathMatch:'full'},
