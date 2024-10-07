e192bb7d1b963da921609716958906c8ca9a5a10
Owusu Ebenezer <121055394+TechOne-Dev@users.noreply.github.com>
2024-10-07 04:27:01 +0000

    complete-task

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,23 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1440px',
+    },
     extend: {
+      container: {
+        center: true,
+      },
       colors: {
-        OnlyUsedTeslaRed: "#E80013",
-        OnlyUsedTeslaRedHover: "#6c7884"
-      }
+        OnlyUsedTeslaRed: '#E80013',
+        OnlyUsedTeslaRedHover: '#6C7884',
+        OnlyUsedTeslaBlue: '#010E25',
+        OnlyUsedTeslaBlack: '#000'
+      },
     },
   },
   plugins: [],
