865c5f33dfaf9efcece57ea4c831bdd0c98eee48
Owusu Ebenezer <121055394+TechOne-Dev@users.noreply.github.com>
2024-10-07 09:00:42 +0000

    feature/initial setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './static/pages/home/home.component';
 import { DashbaordComponent } from './features/dashbaord/dashbaord.component';
 import { UsedTeslaForSaleComponent } from './features/listings/pages/used-tesla-for-sale/used-tesla-for-sale.component';
+import { AboutComponent } from './features/about/about.component';
 
 export const routes: Routes = [
-  {path:'', redirectTo:'home', pathMatch:'full'},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'about', component:AboutComponent},
   {path:'used-tesla-for-sale', component:UsedTeslaForSaleComponent},
   {path:'dashboard', component:DashbaordComponent},
   {path:'home', component:HomeComponent},
